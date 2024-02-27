a2d7403b55ab7ad8cbcf2b2a92874490edc1f5d3
jessicanwey <jessica.n.wey@gmail.com>
2024-02-27 13:25:37 -0500

    Models created

---
+++ models/Thought.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./User');
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtTime) => dateFormat(createdAtTime),
+        },
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtTime) => dateFormat(createdAtTime),
+      },
+      thoughts: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
